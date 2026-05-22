f12b64ed6691c6a61eb111878c5c88b1c1292be9
Gaurav Sharma <gauravsharma16072001@gmail.com>
2026-05-22 15:31:28 +0530

    out-export

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://syntra-dev.duckdns.org/:path*", // ✅ apna actual backend URL
-      },
-    ];
-  },
+    output: 'export',
 };
 
 export default nextConfig;
